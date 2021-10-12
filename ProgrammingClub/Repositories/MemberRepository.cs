ccbf4da93be1690f597b08a8ae0d56a1fae76156
captifs <adrian.bejenaru@stud.acs.upb.ro>
2021-10-12 11:06:48 +0300

    l

---
+++ ProgrammingClub/Repositories/MemberRepository.cs
@@ -0,0 +1,48 @@
+﻿using ProgrammingClub.Models;
+using ProgrammingClub.Models.DBObject;
+using ProgrammingClub.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProgrammingClub.Repositories
+{
+    public class MemberRepository
+    {
+        private ClubMembershipModelsDataContext dbContext;
+        public MemberRepository()
+        {
+            this.dbContext = new ClubMembershipModelsDataContext();
+
+        }
+        public MemberRepository(ClubMembershipModelsDataContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public MemberCodeSnippetsViewModel GetMemberCodeSnippets(Guid memberID)
+        {
+            MemberCodeSnippetsViewModel memberCodeSnippetsViewModel = new MemberCodeSnippetsViewModel();
+            Member member = dbContext.Members.FirstOrDefault(m => m.IDMember == memberID);
+
+            if (member != null)
+            {
+                memberCodeSnippetsViewModel.Name = member.Name;
+                memberCodeSnippetsViewModel.Position = member.Position;
+                memberCodeSnippetsViewModel.Title = member.Title;
+
+                IQueryable<CodeSnippet> memberCodeSnippets = dbContext.CodeSnippets.Where(c => c.IDMember == memberID);
+                foreach (CodeSnippet code in memberCodeSnippets)
+                {
+                    CodeSnippetModel codeSnippetModel = new CodeSnippetModel();
+                    codeSnippetModel.Title = code.Title;
+                    codeSnippetModel.ContentCode = code.ContentCode;
+                    codeSnippetModel.Revision = code.Revision;
+                    memberCodeSnippetsViewModel.CodeSnippets.Add(codeSnippetModel);
+                }
+            }
+            return memberCodeSnippetsViewModel;
+        }
+    }
+}
